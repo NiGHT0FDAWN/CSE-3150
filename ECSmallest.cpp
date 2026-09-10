0ae2cf6dcb972b39ec55a4bc2950398e6b29feee
Joshua <joshua@danieland.net>
2026-09-09 20:57:36 -0400

    :)

---
+++ ECSmallest.cpp
@@ -0,0 +1,8 @@
+// You are given an array of integers (which can be positive or negative). You want to find the smallest integer in this array. If the array is empty, return 0; otherwise return the smallest value. 
+// This tests whether you can do simple loop in C and handle basic logic
+int ECSmallest(int *arrOfInts, int szArr)
+{
+  // arrOfInts: pointer to the array of integers
+  // szArr: number of elements in the array
+  // your code here
+}
---
+++ ECTestTriangle.cpp
@@ -0,0 +1,8 @@
+/* test whether three integers can form a triangle where the given integers are the lengths of the three edges of the triangle */
+/* return 0 if yes, and -1 if not */
+/* note: triangle edge length must be positive (larger than 0) and sum of two sides is strictly larger than the third side */
+int ECTestTriangle(int a, int b, int c)
+{
+  // your code here
+}
+
