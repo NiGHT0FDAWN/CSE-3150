6969ab1f4ecd06aa0be201d132f05e1d8de25af1
Joshua <joshua@danieland.net>
2026-09-09 22:12:53 -0400

    :)

--- ECFindRoot.cpp
+++ ECFindRoot.cpp
@@ -1,10 +1,10 @@
 // This function returns the smallest non-negative integral root of a polynomial (as specified by a list of coefficients and degree) that is no larger than xmax. Return -1 if there is no roots within the range.
 //To be specific: for each integer 0 <=i <= degree, listCoeffs[d] = the coefficient of the degree d term. For example, 
-
+#include <stddef.h>
 int ECFindRoot(int *listCoeffs, int degree, int xmax)
 
 {
-  #include <stddef.h>
+
   // listCoeffs: pointer to the array of integers as the coefficients of a polynomial; listCoeffs[0] is the constant term and so on
   // degree: highest degree term. That is, the number of coefficients in the array = degree+1
   // xmax: the largest value of root to search
